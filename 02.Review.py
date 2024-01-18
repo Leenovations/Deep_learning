847d60a6ce3b2074041d24fff754fd038816c9bb
ltk <xorrb85@gmail.com>
2024-01-18 16:25:01 +0900

    Update

--- 02.Review.py
+++ 02.Review.py
@@ -3,22 +3,21 @@
 import tensorflow as tf
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-
-print('\n' + '#-----------------------------------------------#' + '\n')
 #---------------------------------------------------------------------------------------#
 x = [1,2,3]
-y = [3,6,9]
+y = [1,4,9]
+#---------------------------------------------------------------------------------------#
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
 #---------------------------------------------------------------------------------------#
 W = tf.Variable(tf.random_uniform([1], -1, 1))
 b = tf.Variable(tf.random_uniform([1], -1, 1))
 #---------------------------------------------------------------------------------------#
-X = tf.placeholder(tf.float32, name='X')
-Y = tf.placeholder(tf.float32, name='Y')
-#---------------------------------------------------------------------------------------#
-hypothesis = W * X + b
+hypothesis = W * tf.square(X) + b
 #---------------------------------------------------------------------------------------#
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+# optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 train_op = optimizer.minimize(cost)
 #---------------------------------------------------------------------------------------#
 with tf.Session() as sess:
@@ -28,3 +27,8 @@ with tf.Session() as sess:
         sess.run(train_op, feed_dict={X:x, Y:y})
 
     print(sess.run(hypothesis, feed_dict={X:10}))
+#---------------------------------------------------------------------------------------#
+
+#---------------------------------------------------------------------------------------#
+
+#---------------------------------------------------------------------------------------#
